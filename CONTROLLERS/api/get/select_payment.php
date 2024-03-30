20b58a9553cb4d1bd1db159d09655ca9a06cc61a
faisy11 <mohamedfayas116@gmail.com>
2024-03-30 13:15:19 +0400

    change payment file

--- CONTROLLERS/api/get/select_payment.php
+++ CONTROLLERS/api/get/select_payment.php
@@ -5,6 +5,7 @@ include_once '../../../models/get.php';
 
 $data = json_decode(file_get_contents('php://input'));
 $obj = new Get();
-$result = $obj->select_payment();
+// $result = $obj->select_payment();
+$result='123';
 echo json_encode($result);
 ?>
