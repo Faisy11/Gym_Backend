a33657a61ac798b5117f3f3394ed7481bb29d605
faisy11 <mohamedfayas116@gmail.com>
2024-03-30 13:05:20 +0400

    check 1

--- CONTROLLERS/api/get/select_users.php
+++ CONTROLLERS/api/get/select_users.php
@@ -6,5 +6,5 @@ include_once '../../../models/get.php';
 $data = json_decode(file_get_contents('php://input'));
 $obj = new Get();
 $result = $obj->select_users();
-echo json_encode($result);
+echo json_encode('wrk');
 ?>
